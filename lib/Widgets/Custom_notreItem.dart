3c3cda01e5de07e6eaaafd5e97abe99c88d49200
tasneemwael <tasneem.wael119@gmail.com>
2024-07-01 01:45:30 +0300

    create edit note view

--- lib/Widgets/Custom_notreItem.dart
+++ lib/Widgets/Custom_notreItem.dart
@@ -1,51 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/Views/editNoteViewBody.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 244, 106, 56),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 244, 106, 56),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text(
+                  "Flutter Tips",
+                  style: TextStyle(fontSize: 26),
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text(
+                  "lorem ipsum",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  size: 30,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 24),
               child: Text(
-                "Flutter Tips",
-                style: TextStyle(fontSize: 26),
+                "May21,2022",
+                style: TextStyle(color: Colors.white, fontSize: 16),
               ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: Text(
-                "lorem ipsum",
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                FontAwesomeIcons.trash,
-                size: 30,
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 24),
-            child: Text(
-              "May21,2022",
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
