d77bc66ea8203aa1804eed28754dd07bd1cf7c52
tasneemwael <tasneem.wael119@gmail.com>
2024-06-30 23:13:12 +0300

    button

--- lib/Widgets/Custom_notreItem.dart
+++ lib/Widgets/Custom_notreItem.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class NotesView extends StatelessWidget {
-  const NotesView({super.key});
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
       decoration: BoxDecoration(
-        color: Colors.brown,
+        color: const Color.fromARGB(255, 244, 106, 56),
         borderRadius: BorderRadius.circular(16),
       ),
       child: Column(
--- lib/Widgets/Notes_body.dart
+++ lib/Widgets/Notes_body.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/Views/notes_view.dart';
+import 'package:notes_app/Widgets/Custom_notreItem.dart';
 import 'package:notes_app/Widgets/custom_appBar.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -17,9 +19,23 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
-          NotesView()
+          Expanded(child: NotesListView()),
         ],
       ),
     );
+  }
+}
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context, index) {
+      return const Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        child: NotesItem(),
+      );
+    });
   }
 }
